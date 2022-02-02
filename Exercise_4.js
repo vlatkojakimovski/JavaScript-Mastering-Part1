03a853d5012a6ebf42a8109f21046dc93739256a
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2022-02-02 23:42:31 +0100

    Exercise 4 updated

---
+++ Exercise_4.js
@@ -0,0 +1,22 @@
+//title
+//body
+//views
+//comments
+//  (author, body)
+//isLive
+
+// function BlogPost (title, body, view, comments, isLive)
+
+let post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 10,
+    comments: [
+        { author: 'a',body: 'b'},
+        { author: 'c', body:'d'}
+    ],
+    isLive: true
+};
+
+console.log(post);
